50e2e27fbc558a6647e7f203c797c268666f15c0
eitan <eitan.vichik@gmail.com>
2022-10-11 14:52:18 +0300

    Remove example subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,4 @@ package frc.robot;
 
 public final class Constants {
 
-    public static class ExampleSubsystem {
-        public static final double POWER = 0.5; // [%]
-    }
-
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,11 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.example.ExampleSubsystem;
 import frc.robot.valuetuner.ValueTuner;
 import webapp.Webserver;
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-    public ExampleSubsystem exampleSubsystem = ExampleSubsystem.getInstance();
-    private final XboxController xbox = new XboxController(Ports.Controls.XBOX);
-    private final JoystickButton a = new JoystickButton(xbox, XboxController.Button.kA.value);
+    // The robot's subsystems and commands are defined here...
 
 
     /**
@@ -39,12 +33,12 @@ public class RobotContainer {
 
 
     /**
-    * Use this to pass the autonomous command to the main {@link Robot} class.
-    *
-    * @return the command to run in autonomous
-    */
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
     public Command getAutonomousCommand() {
-      return null;
+        return null;
     }
 
     /**
