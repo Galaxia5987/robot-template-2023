ae7349f058684fe186ac51dd08bfa597f85b6ded
eitan <eitan.vichik@gmail.com>
2022-10-15 13:31:25 +0300

    Revert "Replace izone with integral accumulator"
    This reverts commit f4c3bd78f88a5713fa174826d07c26d38729c116.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-template-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "286b6f303f8263eb92a3f38f79970614960c6495";
-  public static final String GIT_DATE = "2022-10-15 06:26:53 GMT-04:00";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "56db70b6dc6524eb100be003b6ec166d00d02054";
+  public static final String GIT_DATE = "2022-10-11 12:59:32 GMT-04:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2022-10-15 06:27:25 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1665829645963L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2022-10-11 13:00:27 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1665507627572L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
