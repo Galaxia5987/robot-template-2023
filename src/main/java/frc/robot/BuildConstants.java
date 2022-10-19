71df17ce73b71d156f83e3477405f7dcf80f530b
eitan <eitan.vichik@gmail.com>
2022-10-19 15:22:14 +0300

    Write basic lib tests

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-template-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "56db70b6dc6524eb100be003b6ec166d00d02054";
-  public static final String GIT_DATE = "2022-10-11 12:59:32 GMT-04:00";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2022-10-11 13:00:27 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1665507627572L;
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "543a47255fbd50d9c0f37a34ce68f0894b26e85d";
+  public static final String GIT_DATE = "2022-10-18 11:34:23 GMT-04:00";
+  public static final String GIT_BRANCH = "test-lib";
+  public static final String BUILD_DATE = "2022-10-19 08:15:59 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1666181759960L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
