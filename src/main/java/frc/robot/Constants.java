c8579a436ae1a5d7610c7f0ec4b7de2aec157fb6
eitan <eitan.vichik@gmail.com>
2022-10-20 15:09:59 +0300

    Add limelight

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,12 @@
 package frc.robot;
 
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.subsystems.drivetrain.config.SwerveModuleConfigBase;
+import org.photonvision.SimVisionTarget;
 
 import static frc.robot.Ports.SwerveDrive.*;
 
@@ -10,6 +14,8 @@ public final class Constants {
     public static final int TALON_TIMEOUT = 10;
     public static final double NOMINAL_VOLTAGE = 10.0;
     public static final double LOOP_PERIOD = 0.02; // [s]
+    public static final double FIELD_WIDTH = 8.23; // Width of the field. [m]
+    public static final double FIELD_LENGTH = 16.46; // Length of the field. [m]
 
     public static final class SwerveDrive {
         public static final int TICKS_PER_ROTATION_DRIVE_MOTOR = 2048;
@@ -49,6 +55,12 @@ public final class Constants {
                 new Translation2d(-Rx, -Ry),
                 new Translation2d(-Rx, Ry)
         };
+
+        public static final double XY_SLEW_RATE_LIMIT = 3.0;
+        public static final double ROTATION_SLEW_RATE_LIMIT = 6.0;
+
+        public static final double TARGET_ADJUST_Kp = 0.03;
+        public static final double TARGET_ADJUST_Kf = 0.1;
     }
 
     public static final class SwerveModule {
@@ -92,6 +104,30 @@ public final class Constants {
                 .configZeroPosition(ZERO_POSITIONS[3])
                 .configJ(0.115)
                 .build();
+    }
+
+    public static class Vision { //TODO: change for competition
+        public static final int CAM_RESOLUTION_HEIGHT = 480; // Height of camera resolution. [pixel]
+        public static final int CAM_RESOLUTION_WIDTH = 640; // Width of camera resolution. [pixel]
+
+        public static final double CAMERA_HEIGHT = 0.79; // [m]
+        public static final double TARGET_HEIGHT_FROM_GROUND = 2.62; // [m] Pefzener 2.62
+        public static final double BIT_CAMERA_HEIGHT = 1.2; // [m]
+        public static final double BIT_TARGET_HEIGHT_FROM_GROUND = 2.1; // [m] Pefzener 2.62
+        public static final double CAMERA_PITCH = 34.67; // Pitch of the vision. [deg]
+        public static final double DIAG_FOV = 75; // Diagonal FOV. [deg]
+        public static final double LED_RANGE = 6; // Visible range of LEDs. [m]
+        public static final double MIN_TARGET_AREA = 10; // Minimal area of target. [pixel^2]
+        public static final double TARGET_WIDTH = 1.36; // Width of vision target strip. [m]
+        public static final double TARGET_RADIUS = 0.678; // [m]
+
+        public static final Pose2d HUB_POSE = new Pose2d( // Position of the hub relative to the field.
+                new Translation2d(FIELD_LENGTH / 2, FIELD_WIDTH / 2), new Rotation2d());
+        public static final Transform2d CAMERA_TO_ROBOT = new Transform2d(
+                new Translation2d(0.038, 0.171), new Rotation2d()); // Position of the vision relative to the robot.
+
+        public static final SimVisionTarget SIM_TARGET_HUB = new SimVisionTarget( // Hub target for vision simulation.
+                HUB_POSE, TARGET_HEIGHT_FROM_GROUND, TARGET_WIDTH, TARGET_HEIGHT_FROM_GROUND);
     }
 
     public static class Intake {
