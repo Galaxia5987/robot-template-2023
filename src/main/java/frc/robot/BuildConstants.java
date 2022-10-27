b2d7c5e1ab5d6845e8fa4c296d3c2d85409344ba
eitan <eitan.vichik@gmail.com>
2022-10-27 09:53:09 +0300

    Fix unit model test

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-template-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "04f722e9bfd825f48f46147fd2b1325abc3fe85a";
-  public static final String GIT_DATE = "2022-10-26 12:15:29 EDT";
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "c3642e65948f0ff5d3fa17feae2591ba85c0d7f7";
+  public static final String GIT_DATE = "2022-10-26 13:01:27 EDT";
   public static final String GIT_BRANCH = "subsystems";
-  public static final String BUILD_DATE = "2022-10-26 13:00:44 EDT";
-  public static final long BUILD_UNIX_TIME = 1666803644564L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2022-10-27 02:50:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1666853450892L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
