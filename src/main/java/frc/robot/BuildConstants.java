3ca21be44e6a0dface5ad712e60ab4796c4f51ac
eitan <eitan.vichik@gmail.com>
2022-10-28 13:35:55 +0300

    Delete all other subsystems

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-template-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 80;
-  public static final String GIT_SHA = "7689911b98ab075e10d69692138fc84a2f77e84a";
-  public static final String GIT_DATE = "2022-10-28 02:48:13 EDT";
-  public static final String GIT_BRANCH = "subsystems";
-  public static final String BUILD_DATE = "2022-10-28 02:50:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1666939805090L;
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "8b4197ad0ae30a5939f5ef2a48523370b3878d73";
+  public static final String GIT_DATE = "2022-10-28 03:08:10 EDT";
+  public static final String GIT_BRANCH = "new-swerve";
+  public static final String BUILD_DATE = "2022-10-28 06:35:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1666953310251L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/utils/IntegratedUtils.java
+++ src/main/java/frc/robot/utils/IntegratedUtils.java
@@ -3,9 +3,6 @@ package frc.robot.utils;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.vision.Limelight;
 import frc.robot.utils.math.AngleUtil;
 
 /**
@@ -15,35 +12,16 @@ import frc.robot.utils.math.AngleUtil;
  * in the code, and ensure the same information for all commands.
  */
 public class IntegratedUtils {
-    private static final Shooter shooter = Shooter.getInstance();
-    private static final Limelight limelight = Limelight.getInstance();
     private static final SwerveDrive swerveDrive = SwerveDrive.getInstance();
-    private static final Intake intake = Intake.getInstance();
-
-    /**
-     * This function gets the distance to the target in two ways:
-     * vision - if the vision has targets
-     * odometry - if the vision doesn't have targets
-     *
-     * @return the distance to the target. [m]
-     */
-    public static double distanceToTarget() {
-        return limelight.getDistance()
-                .orElse(swerveDrive.getPose()
-                        .minus(Constants.Vision.HUB_POSE)
-                        .getTranslation().getNorm());
-    }
 
     public static double angleToTarget() {
-        return limelight.getYaw().orElseGet(() -> {
-            var toTarget = swerveDrive.getPose().minus(Constants.Vision.HUB_POSE);
-            var absoluteAngleToTarget = new AngleUtil.Angle(
-                    AngleUtil.UP_COUNTER_CLOCKWISE,
-                    Math.toDegrees(Math.atan2(toTarget.getY(), toTarget.getX())));
-                    var robotAngle = new AngleUtil.Angle(
-                    AngleUtil.UP_COUNTER_CLOCKWISE,
-                    Robot.getAngle());
-            return AngleUtil.absoluteAngleToYaw(absoluteAngleToTarget.minus(robotAngle));
-        });
+        var toTarget = swerveDrive.getPose().minus(Constants.Vision.HUB_POSE);
+        var absoluteAngleToTarget = new AngleUtil.Angle(
+                AngleUtil.UP_COUNTER_CLOCKWISE,
+                Math.toDegrees(Math.atan2(toTarget.getY(), toTarget.getX())));
+        var robotAngle = new AngleUtil.Angle(
+                AngleUtil.UP_COUNTER_CLOCKWISE,
+                Robot.getAngle());
+        return AngleUtil.absoluteAngleToYaw(absoluteAngleToTarget.minus(robotAngle));
     }
 }
